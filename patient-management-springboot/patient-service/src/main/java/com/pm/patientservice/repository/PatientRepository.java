752c7ae2dfbfb43af137d3758f025250cf64eb30
Rutuja <patil.rut@northeastern.edu>
2025-04-17 14:29:51 -0400

    mvc architecture

---
+++ patient-management-springboot/patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.pm.patientservice.repository;
+
+import com.pm.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+//jpa repo -> crud operations boiler plate
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+}
